5ff94078eb1024478aafd9a0ed906e8ce69e4a11
Mathias Johansson <MathiasJoha@gmail.com>
2026-06-11 09:18:10 +0200

    Initial commit

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:preload(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- Import LazyVim specifications
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Import pre-configured languages (Python)
+    { import = "lazyvim.plugins.extras.lang.python" },
+    -- Import user custom plugins
+    { import = "plugins" },
+  },
+  defaults = { lazy = false, version = false },
+  checker = { enabled = true },
+})
